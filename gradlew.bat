7c193ce51cc53c2ad173fc0d366a37fdce153001
mike <veangelo@gmail.com>
2026-01-19 00:40:39 +0800

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,6 +1,21 @@
-@echo off
-set DIR=%~dp0
-set APP_HOME=%DIR%
-set APP_NAME=Gradle
+@rem
+@rem Gradle startup script for Windows
+@rem
 
-java -jar "%APP_HOME%\gradle\wrapper\gradle-wrapper.jar" %*
+@if "%DEBUG%"=="" @echo off
+setlocal
+
+set DIRNAME=%~dp0
+if "%DIRNAME%"=="" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+set WRAPPER_JAR=%APP_HOME%gradle\wrapper\gradle-wrapper.jar
+
+if not exist "%WRAPPER_JAR%" (
+  echo ERROR: Could not find %WRAPPER_JAR%
+  echo Please run: gradle wrapper
+  exit /b 1
+)
+
+java -classpath "%WRAPPER_JAR%" org.gradle.wrapper.GradleWrapperMain %*
