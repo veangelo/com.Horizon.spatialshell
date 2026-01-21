e2508de13bf8537c5d0fbd25e8b19ccdbed27677
mike <veangelo@gmail.com>
2026-01-21 16:25:20 +0800

    Update MainActivity.kt

--- app/src/main/java/com/horizon/spatialshell/MainActivity.kt
+++ app/src/main/java/com/horizon/spatialshell/MainActivity.kt
@@ -1,15 +1,32 @@
-class MainActivity : Activity() {
+package com.horizon.spatialshell
+
+import android.opengl.GLSurfaceView
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
 
     private lateinit var glView: GLSurfaceView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        glView = GLSurfaceView(this)
-        glView.setEGLContextClientVersion(3)
-        glView.setRenderer(SpatialRenderer())
-        glView.renderMode = GLSurfaceView.RENDERMODE_CONTINUOUSLY
+        glView = GLSurfaceView(this).apply {
+            setEGLContextClientVersion(2)
+            setRenderer(SpatialRenderer())
+            renderMode = GLSurfaceView.RENDERMODE_CONTINUOUSLY
+        }
 
         setContentView(glView)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        glView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        glView.onResume()
     }
 }
