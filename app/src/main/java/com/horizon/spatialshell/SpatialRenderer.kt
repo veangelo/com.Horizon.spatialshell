2a162106e966dea32c502a7f527993465fd21cb7
mike <veangelo@gmail.com>
2026-01-21 16:25:56 +0800

    Update SpatialRenderer.kt

--- app/src/main/java/com/horizon/spatialshell/SpatialRenderer.kt
+++ app/src/main/java/com/horizon/spatialshell/SpatialRenderer.kt
@@ -1,14 +1,21 @@
+package com.horizon.spatialshell
+
+import android.opengl.GLES20
+import android.opengl.GLSurfaceView
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
+
 class SpatialRenderer : GLSurfaceView.Renderer {
 
-    private var time = 0f
+    override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
+        GLES20.glClearColor(0.05f, 0.05f, 0.08f, 1.0f)
+    }
+
+    override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
+        GLES20.glViewport(0, 0, width, height)
+    }
 
     override fun onDrawFrame(gl: GL10?) {
-        time += 0.016f
-
-        val floatY = kotlin.math.sin(time) * 0.04f
-        val floatX = kotlin.math.cos(time) * 0.02f
-
-        // Use floatX, floatY to move your panel matrix
-        drawFloatingPanel(floatX, floatY)
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT or GLES20.GL_DEPTH_BUFFER_BIT)
     }
 }
